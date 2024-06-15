759b3fee363be3a782d703e34afaf96b39003c8e
Casey Holman <36244725+afishofsorts@users.noreply.github.com>
2024-06-15 16:56:09 -0500

    Add files via upload

--- WanderingLine/sigGen.py
+++ WanderingLine/sigGen.py
@@ -1,5 +1,5 @@
 import numpy as np
-from numpy import random
+import random as rand
 import freqGen as fg
 from scipy.interpolate import splrep, BSpline
 import matplotlib.pyplot as plt
@@ -15,17 +15,26 @@ def freqToPhase(freq):
 
     return phase
 
+def addVar(data, stnd: float):
+    newdata = np.zeros(len(data))
+
+    for i in range(len(data)):
+        dy = rand.gauss(data[i], stnd)
+        newdata[i] = data[i] + dy
+    return newdata
+
 def genSignal(f0: float, band: float, A: float, M = 20):
     p = fg.genKnot(M, f0, band)
     freq = fg.genBSpline(p)
     phase = freqToPhase(freq)
     cleanSig = A*np.cos(phase[:, 1])
-    return [phase[:, 0], cleanSig]
+    distSig = addVar(cleanSig, 0.3*A)
+    return [phase[:, 0], distSig]
 
 f0 = 60; band = 30; A = 2
 signal = genSignal(f0, band, A)
 
 fig = plt.figure(figsize=(15, 5))
-plt.plot(signal[0], signal[1], '-')
+plt.plot(signal[0], signal[1], 'o')
 plt.xlabel('t (s)'); plt.ylabel('q(t)')
 plt.show()
